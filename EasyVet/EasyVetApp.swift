ce1adc30fcd3855890100ae62fbcf4a7e901092f
jmayta1984 <jmayta@pucp.edu.pe>
2026-05-09 09:31:56 -0500

    chore: first commit

---
+++ EasyVet/EasyVetApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyVetApp.swift
+//  EasyVet
+//
+//  Created by Jorge Mayta on 11/04/26.
+//
+
+import SwiftUI
+
+@main
+struct EasyVetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
